e9e564b41559d722940f7152ed1c35c904f449e9
mk9202 <mk9202@g.rit.edu>
2024-04-16 17:21:35 -0400

    Added code to the GUI

--- GUI.py
+++ GUI.py
@@ -1,3 +1,32 @@
-import tkinter as tk
-
-window = tk.Tk()
+# Python program to create a table
+  
+from tkinter import *
+from cache import *
+ 
+ 
+class Table:
+     
+    def __init__(self,root):
+         
+        # code for creating table
+        for i in range(total_rows):
+            for j in range(total_columns):
+                 
+                self.e = Entry(root, width=20, fg='blue',
+                               font=('Arial',16,'bold'))
+                 
+                self.e.grid(row=i, column=j)
+                self.e.insert(END, lst[i][j])
+ 
+# swap with cache the data
+lst = cache
+  
+# find total number of rows and
+# columns in list
+total_rows = len(lst)
+total_columns = len(lst[0])
+  
+# create root window
+root = Tk()
+t = Table(root)
+root.mainloop()
