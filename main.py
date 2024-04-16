19d061e9cd73e6c9e58e55cf7c3f6b26618828d1
LJL7823 <ljl7823@rit.edu>
2024-04-16 15:35:33 -0400

    Uncommented the Code

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import sys
 def main():
     print("Cache Simulator by Ruhan, Maya, Key'Mon, and Liam \n\n")
     # Prompts the user for each required input
-    """size: int = input("Input the Nominal Size of the cache in Bytes:")
+    size: int = input("Input the Nominal Size of the cache in Bytes:")
     words_per_block : int = input("Input the number of words per block (1, 2, 4, 8,...):")
     mapping_policy : str = input("Mapping type Direct (D) or Set-Associative (S):")
     if(mapping_policy == 'S' or mapping_policy == 's'):
@@ -15,7 +15,7 @@ def main():
     else:
         # Should be replaced with the create of the cache
         print(size,words_per_block, mapping_policy)
-    """
+    
     
     # This is me f*ing around with the input loop
     while (True):
