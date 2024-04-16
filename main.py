165d03ece6cb5ea10916de3cda7a9e543e8720f4
LJL7823 <ljl7823@rit.edu>
2024-04-16 16:41:24 -0400

    Added stuff for Set Associative

--- main.py
+++ main.py
@@ -4,24 +4,34 @@ import sys
 import math as m
 
 def main():
+    """Main Method of the program does the I/O
+    """
     print("Cache Simulator by Ruhan, Maya, Key'Mon, and Liam \n\n")
     # Prompts the user for each required input
-    size: int = input("Input the Nominal Size of the cache in Bytes:")
-    words_per_block : int = input("Input the number of words per block (1, 2, 4, 8,...):")
+    nom_size: int = input("Input the Nominal Size of the cache in Bytes:") # type: ignore
+    words_per_block : int = input("Input the number of words per block (1, 2, 4, 8,...):") # type: ignore
     mapping_policy : str = input("Mapping type Direct (D) or Set-Associative (S):")
     if(mapping_policy == 'S' or mapping_policy == 's'):
-        blocks_per_set = input("input the number of blocks per set:")
+        theN : int = input("input the number of blocks per set:") # type: ignore
+        bytesPerBlock : int = 4 * words_per_block # solves for the number of the bytes per block
+        num_block : int = (nom_size//bytesPerBlock)   # number of blocks 
+        num_set : int = (num_block//theN)
+        index_bits :int = int(m.log2(num_set))
+        offset_bits :int  = int(m.log2(bytesPerBlock))  # the offset in bits 
+        tag : int = 32 - (index_bits + offset_bits)   # the tag in bits
+        stat : int = 1 + int(m.log2(theN))
+        realSize : int = (nom_size + num_block*((tag + stat)//8))//(2^10)  # solves for the real size in kilobytes
         # Should be replace with the creation of the cache
-        print(size,words_per_block, mapping_policy, blocks_per_set)
+        print(nom_size,words_per_block, mapping_policy, num_set)
     else:
         # Should be replaced with the create of the cache
         bytesPerBlock : int = 4 * words_per_block # solves for the number of the bytes per block
-        num_block : int = (size//bytesPerBlock)   # number of blocks 
+        num_block : int = (nom_size//bytesPerBlock)   # number of blocks 
         index_bits : int = int(m.log2(num_block))   # the index in bits 
         offset_bits :int  = int(m.log2(bytesPerBlock))  # the offset in bits 
         tag : int = 32 - (index_bits + offset_bits)   # the tag in bits
-        realSize : int = (size + num_block*((tag + 1)//8))//(2^10)  # solves for the real size in kilobytes
-        print(size,words_per_block, mapping_policy, bytesPerBlock, num_block, index_bits, offset_bits, tag, realSize)
+        realSize : int = (nom_size + num_block*((tag + 1)//8))//(2^10)  # solves for the real size in kilobytes
+        print(nom_size,words_per_block, mapping_policy, bytesPerBlock, num_block, index_bits, offset_bits, tag, realSize)
 
 
     
